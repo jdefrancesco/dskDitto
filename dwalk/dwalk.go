302be8f259f07da174612d4e477112e4bc5a3542
jdefr89 <jdefr89@gmail.com>
2021-06-14 15:24:40 -0400

    Minor changes to README.md

--- dwalk/dwalk.go
+++ dwalk/dwalk.go
@@ -98,8 +98,7 @@ func walkDir(ctx context.Context, dir string, d *DWalk, dFiles chan<- *dfs.Dfile
 			// Handle special files...
 			if !entry.Mode().IsRegular() {
 				// For now we will skip over special files...
-				// XXX: TURN THIS BACK ON
-				// log.Info().Msgf("Skipping file %s", entry.Name())
+				log.Info().Msgf("Skipping file %s", entry.Name())
 				continue
 			}
 
