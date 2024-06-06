775eca3cedea04875fb3700c5da31f61699c655c
jdefr89 <jdefr89@gmail.com>
2024-06-05 20:29:15 -0400

    Handle marking files in tree
    User presses 'm' to mark or unmark a file in order
    for it to be processed, i.e deleted possibly.
    
    Got rid of zerolog, just using log for now.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"runtime/pprof"
@@ -18,10 +19,7 @@ import (
 	"github.com/pterm/pterm"
 	"github.com/pterm/pterm/putils"
 	"github.com/rivo/tview"
-	"github.com/rs/zerolog"
 )
-
-var fileLogger zerolog.Logger
 
 func init() {
 
@@ -71,6 +69,16 @@ func main() {
 	)
 	flag.Parse()
 
+	// Open a file for logging
+	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		log.Fatal("Failed to open log file: ", err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	log.Println("==================== dskDitto Started ====================")
+
 	// TODO: Not implemented yet.
 	_ = flMaxFileSize
 
@@ -78,7 +86,7 @@ func main() {
 	if *flCpuProfile != "" {
 		f, err := os.Create(*flCpuProfile)
 		if err != nil {
-			fileLogger.Fatal().Err(err).Msgf("cpuprofile failed")
+			log.Fatal("cpuprofile failed")
 		}
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
@@ -102,7 +110,8 @@ func main() {
 	// Dmap stores duplicate file information.
 	dMap, err := dmap.NewDmap()
 	if err != nil {
-		fileLogger.Fatal().Msgf("could not create new dmap: %s\n", err)
+		log.Println("Failed to make new Dmap: ", err)
+		return
 	}
 
 	// dFiles will be the channel we receive files to be added to the DMap.
@@ -182,16 +191,54 @@ MainLoop:
 	// Add the nodes to the tree.
 	addTreeData(tree, dMap)
 
+	// Map to keep track of marked items
+	markedItems := make(map[string]*tview.TreeNode)
+
 	// Key binding to quit.
 	tree.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
 		case tcell.KeyEsc:
 			app.Stop()
 		case tcell.KeyRune:
-			if event.Rune() == 'q' {
+			switch event.Rune() {
+			case 'q':
 				app.Stop()
+
+			// Handle marking items for deletion
+			case 'm':
+				log.Println("Marking item")
+				currentNode := tree.GetCurrentNode()
+				log.Printf("Current node: %d", currentNode.GetLevel())
+				// Skip selection of root node for now.
+				if currentNode.GetLevel() == 1 {
+					goto Skip
+				}
+
+				if node, ok := markedItems[currentNode.GetText()]; !ok {
+					markedItems[currentNode.GetText()] = currentNode
+					log.Printf("Marked item: %v", markedItems)
+					currentNode.SetColor(tcell.ColorYellow)
+				} else {
+					delete(markedItems, node.GetText())
+					node.SetColor(tcell.ColorWhite)
+					log.Printf("Unmarking item: %v", markedItems)
+				}
+
+			case 'd':
+				// for path, node := range markedItems {
+				// 	err := os.Remove(path)
+				// 	if err != nil {
+				// 		log.Printf("Failed to delete file: %s", err)
+				// 	} else {
+				// 		node.SetColor(tcell.ColorGray).SetText("[Deleted] " + filepath.Base(path))
+				// 	}
+				// }
+				// markedItems = make(map[string]*tview.TreeNode) // Clear marked items
+				app.Draw()
 			}
+
 		}
+	Skip:
 		return event
 	})
 
@@ -204,7 +251,10 @@ MainLoop:
 		}
 	})
 
-	if err := app.SetRoot(tree, true).EnableMouse(true).Run(); err != nil {
+	// Launch the TUI.
+	if err := app.SetRoot(tree, true).
+		EnableMouse(true).
+		Run(); err != nil {
 		panic(err)
 	}
 
